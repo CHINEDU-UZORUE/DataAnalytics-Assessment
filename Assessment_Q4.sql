b68444a2a53a0779d468bf300c3a9deb6435c969
CHINEDU UZORUE <53054791+CHINEDU-UZORUE@users.noreply.github.com>
2025-05-19 11:41:56 +0100

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -8,9 +8,10 @@ USE adashi_staging;
 
 -- CTE to get average profit_per_transaction per customer
 WITH appt AS (
-SELECT owner_id,
-COUNT(transaction_reference) AS total_transactions,
-(0.001*SUM(confirmed_amount)) / COUNT(transaction_reference) AS avg_profit_per_transaction
+SELECT 
+	owner_id,
+	COUNT(transaction_reference) AS total_transactions,
+	(0.001*SUM(confirmed_amount)) / COUNT(transaction_reference) AS avg_profit_per_transaction
 FROM savings_savingsaccount
 GROUP BY owner_id
 )
@@ -19,7 +20,7 @@ SELECT
 	u.id AS owner_id,
 	CONCAT(u.first_name, ' ', u.last_name) AS name,
 	TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_months,
-    a.total_transactions,
+    	a.total_transactions,
     ROUND((a.total_transactions / (TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()))) * 12 * avg_profit_per_transaction,2) AS estimated_clv
 FROM users_customuser AS u
 JOIN appt AS a ON u.id = a.owner_id
